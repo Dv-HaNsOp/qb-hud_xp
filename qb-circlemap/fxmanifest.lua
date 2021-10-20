e0428a6b5ad2058fad2381f474029af3aa1ea184
HaNs Op Yt <teamhans42@gmail.com>
2021-10-20 22:10:42 +0530

    qb-hud

---
+++ qb-circlemap/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+
+url "https://www.zerodream.net/"
+version "1.2"
+
+client_script 'client.lua'
+server_script 'server.lua'
+
+files {
+	"hud.html"
+}
+
+ui_page "hud.html"
